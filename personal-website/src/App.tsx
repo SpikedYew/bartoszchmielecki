26bdc5fff2bfdb56239a750c3d09ebde441db63f
Bartosz <bartoszchmielecki2003@gmail.com>
2025-03-10 22:27:49 +0100

    ! portfolio

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
+import { Instagram, Facebook } from "lucide-react";
+import Menu from "./Comp/Menu";
 
 function App() {
- 
-
   return (
     <>
-<div className='w-screen h-screen flex text-center items-center justify-center text-7xl font-semibold'>Bartosz Chmielecki</div>
+      <div className="flex flex-row">
+        <Menu />
+        <div className="flex flex-col w-full">
+          <div className=" h-[100vh] flex flex-col items-center justify-center text-7xl font-semibold">
+            <div className="w-[620px]">
+              <h1>Bartosz Chmielecki</h1>
+              <h2 className="text-3xl mt-5">
+                Strony Internetowe generujące zasięgi
+              </h2>
+            </div>
+            <div className="flex h-[100px] w-[620px] items-center ">
+              {" "}
+              <Instagram className="mr-12 transition-all hover:mb-4" />
+              <Facebook className="mr-12 transition-all hover:mb-4" />
+            </div>
+          </div>
+          <div className="  flex flex-col items-center justify-center text-5xl font-semibold">
+            <div className="w-[620px]">
+              <h1>Realizacje</h1>
+              <ul>
+                <li className="text-2xl mt-3" id="1">
+                  mfcstudio.pl
+                </li>
+                <li className="text-2xl mt-3">telesystemspj.pl</li>
+                <li className="text-2xl mt-3">studiouszko.pl</li>
+                <li className="text-2xl mt-3">studiouszko.pl</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ personal-website/src/Comp/Menu.tsx
@@ -0,0 +1,19 @@
+export default function Menu() {
+  return (
+    <>
+      <div className="w-[25vw] h-screen shadow-2xl flex-col sticky top-0 flex items-center justify-center">
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold mb-2">Menu</h1>
+          <ul>
+            <li>
+              <a href="#1">Realizacje</a>
+            </li>
+            <li>Dlaczego ja?</li>
+            <li>Darmowa audyt i wycena</li>
+            <li>Darmowa audyt i wycena</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
